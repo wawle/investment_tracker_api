63228fa95eb215a80319b8fbe411da50bc0937dd
wawle <berkayyucel96@hotmail.com>
2025-03-11 18:09:21 +0300

    user model updated

--- src/models/User.ts
+++ src/models/User.ts
@@ -70,7 +70,7 @@ const UserSchema: Schema<IUserModal> = new Schema(
 // Sign JWT and return
 UserSchema.methods.getSignedJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET as string, {
-    expiresIn: process.env.JWT_EXPIRE,
+    expiresIn: process.env.JWT_EXPIRE || "1h",
   });
 };
 
